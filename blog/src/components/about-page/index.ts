491f43be5552217fc7d35f4650132eb88f55a217
Anton Gruzdev <tauit895031@gmail.com>
2025-04-30 19:42:55 +0300

    finish update

---
+++ blog/src/components/about-page/index.ts
@@ -0,0 +1,5 @@
+export { AboutCard } from './about-card';
+export { AboutMe } from './about-me';
+export { Skills } from './skills';
+export { Education } from './eductaion';
+export { Experience } from './experience';

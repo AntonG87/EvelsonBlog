86910ef91ada32ff24b3225429f4291f475b87f3
Anton Gruzdev <tauit895031@gmail.com>
2025-04-30 20:51:54 +0300

    finish update

--- blog/src/components/post-submission-success.tsx
+++ blog/src/components/post-submission-success.tsx
@@ -2,7 +2,7 @@ export const PostSubmissionSuccess = () => {
   return (
     <div className="text-center py-8">
       <h2 className="text-2xl font-bold">Congratulations, your post has been published on our service</h2>
-      <img src="/succes.jpg" alt="Success" className="mx-auto max-w-[400px] mt-4" />
+      <img src="/succes.jpg" alt="Success" className="mx-auto max-w-[400px] max-sm:w-[200px] mt-4" />
       <p className={'mt-4'}>Thank you for your contribution!</p>
     </div>
   );
